3296e6272abd3549fd78b0f78fbe33ba08300562
danielszymak <danielszymak@gmail.com>
2019-09-10 23:24:35 +0200

    thats_all

--- src/index.js
+++ src/index.js
@@ -2,22 +2,6 @@ import "./js/search.js"
 import "./js/script.js"
 import "./css/style.css"
 
-var button1 = document.getElementById("showWeather").addEventListener('click', buttonClick);
-
-function buttonClick(){
-    document.querySelector('.title').style.display= 'none';
-    document.querySelector('.content').style.display= 'block';
-    //document.getElementById('otherCity').style.display='block';
-    //document.getElementById('settings').style.display='grid';
-    //if (document.querySelector('.precipitation').textContent == '0.0 mm/h ') {
-        //document.querySelector('.weatherPresentation').style.backgroundImage = 'url(./src/photos/slonecznapogoda2.jpg)';
-        //document.querySelector('.weatherPresentation').style.color = 'black';
-    //} else if (document.querySelector('.precipitation').textContent != '0.0 mm/h ') {
-        //document.querySelector('.weatherPresentation').style.backgroundImage = 'url(./src/photos/deszcz1.jpg)';
-        //document.querySelector('.weatherPresentation').style.color = 'white';
-    //}
-}
-
 
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 
 const endpoint = 'https://raw.githubusercontent.com/cc-team-2019/weather-APP/master/src/miasta.json';
 const cities = [];
+const kelvin = 273
+const key = "82005d27a116c2880c8f0fcb866998a0"
 let searchedLat;
 let searchedLon;
 const notificationElement = document.querySelector('.notification')
@@ -38,12 +40,23 @@ function displayMatches() {
  
 }
 
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(setPosition, showError)
-} else {
-    console.log('geolocation not available')
+//------------------------------
+if(typeof(Storage) !== "undefined" && localStorage.getItem("lat")!==null){
+    let latitude = localStorage.getItem("lat");
+    let longitude = localStorage.getItem("lon");
+    getWeather(latitude, longitude);
+    console.log("Default: "+localStorage.getItem("lat")+" "+localStorage.getItem("lon"));
 }
+else{
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(setPosition, showError)        
+    } else {
+        console.log('geolocation not available')
+    }
+}
+//----------------------
 
+//GEO start
 function setPosition(position) {
     let latitude = position.coords.latitude
     let longitude = position.coords.longitude
@@ -54,6 +67,28 @@ function showError(error) {
     notificationElement.style.display = 'block';
     notificationElement.innerHTML = `<p>${error.message}</p>`
 }
+//End of GEO
+
+
+//Storage---------------------------
+document.getElementById("button_set").addEventListener('click', isSetDefault);
+
+function isSetDefault(){
+    if (typeof(Storage) !== "undefined"){
+        localStorage.setItem("lat", searchedLat);
+        localStorage.setItem("lon", searchedLon);
+        console.log("lat "+localStorage.getItem("lat")+", long "+localStorage.getItem("lon"))
+    }
+}
+
+document.getElementById("button_delete").addEventListener('click', deleteDefault);
+
+function deleteDefault(){
+    localStorage.removeItem("lat");
+    localStorage.removeItem("lon");
+}
+//---------------------------------------
+
 
 const searchInput = document.querySelector('.search>input')
 const suggestions = document.querySelector('.suggestions')
@@ -70,8 +105,7 @@ searchInput.addEventListener('change', displayMatches)
 searchInput.addEventListener('keyup', displayMatches)
  
 
-const kelvin = 273
-const key = "82005d27a116c2880c8f0fcb866998a0"
+
 
 function directionFinder(deg) {
     if (deg<45 || deg>315) {
@@ -87,19 +121,19 @@ function directionFinder(deg) {
 
 function backgroundFinder(description) {
     if (description == 'clear sky' || description == 'few clouds') {
-        return 'url(./photo/sun.jpg)'
+        return 'url(./src/photo/sun.jpg)'
     } else if (description == 'snow') {
-        return 'url(./photo/snow.jpg)'
+        return 'url(./src/photo/snow.jpg)'
     } else if (description == 'shower rain' || description == 'rain' || description == 'mist') {
-        return 'url(./photo/rain.jpg)'
+        return 'url(./src/photo/rain.jpg)'
     } else if (description == 'thunderstorm') {
-        return 'url(./photo/lightning.jpg)'
-    } else return 'url(./photo/darkclouds.jpg)';
+        return 'url(./src/photo/lightning.jpg)'
+    } else return 'url(./src/photo/darkclouds.jpg)';
 }
 
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
-    let apiDays = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
+    let apiDays = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
 
     notificationElement.style.display = 'none';
 
@@ -112,7 +146,7 @@ function getWeather(latitude, longitude) {
             icon.parentElement.style.setProperty('--tran0', `${directionFinder(data.wind.deg)}(0deg)`)
             icon.parentElement.style.setProperty('--tran1', `${directionFinder(data.wind.deg)}(360deg)`)
             icon.parentElement.style.animationDuration = `${15/Math.floor(data.wind.speed)}s`
-            icon.setAttribute('src', `./icons/${data.weather[0].icon}.svg`)
+            icon.setAttribute('src', `./src/icons/${data.weather[0].icon}.svg`)
             tempValue.innerText = `${data.main.temp}° C`
             description.innerText = data.weather[0].description
             locale.innerText = `${data.name}, ${data.sys.country}`
@@ -126,13 +160,11 @@ function getWeather(latitude, longitude) {
             return data
         })
         .then(data => {
-            console.log(hours)
-            console.log(data)
             for (i=0; i<hours.length; i++) {
                 hours[i].children[0].style.setProperty('--tran0', `${directionFinder(data.list[i+1].wind.deg)}(0deg)`)
                 hours[i].children[0].style.setProperty('--tran1', `${directionFinder(data.list[i+1].wind.deg)}(360deg)`)
                 hours[i].children[0].style.animationDuration = `${15/Math.floor(data.list[i+1].wind.speed)}s`
-                hours[i].children[0].firstChild.setAttribute('src', `./icons/${data.list[i+1].weather[0].icon}.svg`)
+                hours[i].children[0].firstChild.setAttribute('src', `./src/icons/${data.list[i+1].weather[0].icon}.svg`)
                 hours[i].children[1].innerText = `${data.list[i+1].main.temp}° C`
                 hours[i].children[2].innerText = data.list[i+1].weather[0].description
                 hours[i].children[3].innerText = data.list[i+1].dt_txt.slice(11,16)
@@ -141,28 +173,12 @@ function getWeather(latitude, longitude) {
                 days[i].children[0].style.setProperty('--tran0', `${directionFinder(data.list[7+i*8].wind.deg)}(0deg)`)
                 days[i].children[0].style.setProperty('--tran1', `${directionFinder(data.list[7+i*8].wind.deg)}(360deg)`)
                 days[i].children[0].style.animationDuration = `${15/Math.floor(data.list[7+i*8].wind.speed)}s`
-                days[i].children[0].firstChild.setAttribute('src', `./icons/${data.list[7+i*8].weather[0].icon}.svg`)
+                days[i].children[0].firstChild.setAttribute('src', `./src/icons/${data.list[7+i*8].weather[0].icon}.svg`)
                 days[i].children[1].innerText = `${data.list[7+i*8].main.temp}° C`
                 days[i].children[2].innerText = data.list[7+i*8].weather[0].description
                 days[i].children[3].innerText = data.list[7+i*8].dt_txt.slice(5,10)
             }
         })
-}
-
-var button1 = document.getElementById("showWeather").addEventListener('click', buttonClick);
-
-function buttonClick(){
-    document.querySelector('.title').style.display= 'none';
-    document.querySelector('.content').style.display= 'block';
-    //document.getElementById('otherCity').style.display='block';
-    //document.getElementById('settings').style.display='grid';
-    //if (document.querySelector('.precipitation').textContent == '0.0 mm/h ') {
-        //document.querySelector('.weatherPresentation').style.backgroundImage = 'url(./src/photos/slonecznapogoda2.jpg)';
-        //document.querySelector('.weatherPresentation').style.color = 'black';
-    //} else if (document.querySelector('.precipitation').textContent != '0.0 mm/h ') {
-        //document.querySelector('.weatherPresentation').style.backgroundImage = 'url(./src/photos/deszcz1.jpg)';
-        //document.querySelector('.weatherPresentation').style.color = 'white';
-    //}
 }
 
 
